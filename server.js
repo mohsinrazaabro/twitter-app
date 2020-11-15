94ad4b7b60781cc23bdffa035cb8541549b0365e
mohsinrazaabro <mohsinrazaaro@gmail.com>
2020-11-14 23:07:04 -0800

    make it better

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static('public'))
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(__dirname + '/public/'))
 
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+    //app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
 //routes
